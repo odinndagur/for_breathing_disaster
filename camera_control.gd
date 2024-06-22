8e905938cb9d564fcc18958aace7922a0a0b67bc
Óðinn Dagur Bjarnason <odinnd@gmail.com>
2024-06-22 23:40:45 +0000

    eyyynett

--- camera_control.gd
+++ camera_control.gd
@@ -1,4 +1,4 @@
-extends Node3D
+extends CharacterBody3D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,3 +9,12 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+#var gravity = 10.0
+var speed = 3.0
+
+func _physics_process(delta):
+	velocity.y += delta * Input.get_axis('move_down', 'move_up') * speed * 0.6
+	velocity.x = delta * Input.get_axis("move_left", "move_right") * speed * 15
+#	look_at(Vector3(0,0,0))
+	move_and_slide()
---
+++ fps_label.gd
@@ -0,0 +1,14 @@
+extends Label
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+func _process(delta):
+	text = ""
+	text += "fps: " + str(Engine.get_frames_per_second())
+
+
